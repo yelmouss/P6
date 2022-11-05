86d51f3c9be4d4c22fece601f9842909db45ab6f
yelmouss <yassine.elmouss@sfr.fr>
2022-11-05 14:27:38 +0100

    tester les requêtes après traitement par middleware & améliorer server.js

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
 module.exports = app;
