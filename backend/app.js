2406e00dd7ce19c52b2a093db8594f46bc4e69c4
yelmouss <yassine.elmouss@sfr.fr>
2022-11-05 15:50:46 +0100

    Config hash pass + AUTH

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/User');
 
 mongoose.connect('mongodb+srv://yelmouss:Mos1969@yelmousscluster.rxp3jsa.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -9,50 +14,20 @@ mongoose.connect('mongodb+srv://yelmouss:Mos1969@yelmousscluster.rxp3jsa.mongodb
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+    const app = express();    
 
-
-
-app.use('/api/sauces', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        userId : "String",
-        name : "String",
-        manufacturer : "String",
-        description : "String",
-        mainPepper : "String",
-        imageUrl : "String",
-        heat : "1",
-        likes :" 1",
-        dislikes :"1",
-        usersLiked : "array",
-        usersDisliked : "array",
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        userId : "String",
-        name : "String",
-        manufacturer : "String",
-        description : "String",
-        mainPepper : "String",
-        imageUrl : "String",
-        heat : "1",
-        likes :" 1",
-        dislikes :"1",
-        usersLiked : "array",
-        usersDisliked : "array",
-      },
-    ];
-    res.status(200).json(stuff);
-  });
-
-
-module.exports = app;
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        next();
+    });
+    
+    app.use(bodyParser.json());
+    
+    // app.use('/images', express.static(path.join(__dirname, 'images')));    
+    // app.use('/api/sauces', saucesRoutes);
+    app.use('/api/auth', userRoutes);
+    
+    module.exports = app;
